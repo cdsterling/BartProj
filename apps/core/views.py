d0d2ae96805dfd54c6d0c41049077288b931570a
Chad Sterling <cdsterling@gmail.com>
2019-12-03 22:36:52 -0800

    started outlining the views for the core app

--- apps/core/views.py
+++ apps/core/views.py
@@ -15,3 +15,15 @@ def about(request):
 
     return render(request, 'pages/about.html', context)
 
+def faq(request):
+    context = {
+
+    }
+
+    return render(request, 'pages/faq.html', context)
+
+def station_selected(request, stationID):
+    #call the api using the station id
+    # get the list of times
+    # put the times into the context
+    #return render(request, 'pages/station.html', context)
