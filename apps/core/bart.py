baba1f4f1a16b22492c50f394a8c3775a5e9da3b
Josh Thompson <jpthompson93@gmail.com>
2019-12-04 20:09:38 -0800

    station arrivals function for bart api

--- apps/core/bart.py
+++ apps/core/bart.py
@@ -1,10 +1,22 @@
 import requests
 import os
 from datetime import datetime, timedelta
-from .models import StationArrival
+import pprint
+bart_key = os.getenv("BART_API_KEY")
 
 # BART API 
 def station_arrivals(): 
+    """
+    KEY DEFINITIONS
+    station_name - station where train is arriving
+    station_abbr - abbreviated station name where train is arriving
+    destination_name - final destination of train
+    destination_abbr - abbreviated final destination of train
+    arrival_minutes - minutes until train arrives
+    arrival_datetime - arrival time of the train
+    """
+
+
     params = {'key': bart_key, 'cmd': 'etd', 'orig': 'ALL', 'json': 'y'}
     response = requests.get('http://api.bart.gov/api/etd.aspx', params=params)
     print(f'Request status code: {response.status_code}')
@@ -14,25 +26,28 @@ def station_arrivals():
         datetime.strptime(result['date'], "%m/%d/%Y"),
         datetime.strptime(result['time'][:7],"%I:%M:%S").time()
     )
+    arrivals = {}
     for station in result['station']: 
+        arrivals[station['abbr']] = []
         for destination in station['etd']: 
             for train in destination['estimate']: 
-                StationArrival.objects.create(
-                    update_datetime=update_datetime,
-                    station_name=station['name'],
-                    station_abbr=station['abbr'],
-                    destination_name=destination['destination'],
-                    destination_abbr=destination['abbreviation'],
-                    arrival_minutes=train['minutes'],
-                    arrival_datetime=estimate_arrival(update_datetime, train['minutes']), 
-                    platform=train['platform'],
-                    direction=train['direction'],
-                    length=train['length'],
-                    color=train['color'],
-                    hexcolor=train['hexcolor'], 
-                    bikeflag=train['bikeflag'],
-                    delay_seconds=train['delay'],
-                )
+                arrivals[station['abbr']].append({
+                    'update_datetime': update_datetime,
+                    'station_name': station['name'],
+                    'station_abbr': station['abbr'],
+                    'destination_name': destination['destination'],
+                    'destination_abbr': destination['abbreviation'],
+                    'arrival_minutes': train['minutes'],
+                    'arrival_datetime': estimate_arrival(update_datetime, train['minutes']), 
+                    'platform': train['platform'],
+                    'direction': train['direction'],
+                    'length': train['length'],
+                    'color': train['color'],
+                    'hexcolor': train['hexcolor'], 
+                    'bikeflag': train['bikeflag'],
+                    'delay_seconds': train['delay'],   
+                })
+    return arrivals
 
 def estimate_arrival(update_datetime, arrival_minutes): 
     try: 
@@ -42,7 +57,10 @@ def estimate_arrival(update_datetime, arrival_minutes):
     return update_datetime + timedelta(minutes=arrival)
 
 def main(): 
-    station_arrivals()
+    arrivals = station_arrivals()
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(arrivals)
+
 
 if __name__ == '__main__': 
     main()
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,29 +1,2 @@
 from django.db import models
 
-class StationArrival(models.Model):
-    """
-    Examples on how to use the data: 
-
-        # pull all data 
-        arrivals = [row.station_abbr for row in StationArrival.objects.all()]
-
-        # get a list of distinct station name abbreviations
-        stations = list(set(station for station in arrivals['station_abbr']))
-
-    Some observations about the API: 
-        - arrival_minutes will sometimes return "Leaving," presumably when the arrival_minutes is <= 0
-    """
-    update_datetime = models.DateTimeField()
-    station_name = models.CharField(max_length=100)
-    station_abbr = models.CharField(max_length=30)
-    destination_name = models.CharField(max_length=100)
-    destination_abbr = models.CharField(max_length=30)
-    arrival_minutes = models.CharField(max_length=30)
-    arrival_datetime = models.DateTimeField()
-    platform = models.CharField(max_length=30)
-    direction = models.CharField(max_length=30)
-    length = models.CharField(max_length=30)
-    color = models.CharField(max_length=30)
-    hexcolor = models.CharField(max_length=30)
-    bikeflag = models.CharField(max_length=30)
-    delay_seconds = models.IntegerField()
