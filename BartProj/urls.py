2d717cff7df76dc2c8fd7813a99799c6771b1591
marby <marbysneaker@gmail.com>
2019-12-08 13:55:48 -0800

    modified home, sigup, login page to work on server

--- BartProj/urls.py
+++ BartProj/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from apps.accounts import views
 
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('account/', include('apps.accounts.urls')),
     path('admin/', admin.site.urls),
-
+    path('signup/', views.sign_up, name='signup'),
+    path('login/',views.login, name='login')
 ] + static( # Add in uploaded media files
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
