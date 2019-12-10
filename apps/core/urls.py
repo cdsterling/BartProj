320c6520b796bb776a37e29626376d220e94c88c
Chad Sterling <cdsterling@gmail.com>
2019-12-09 20:37:24 -0800

    updates for chad views

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,7 +5,7 @@ from apps.core import views
 '.site.com/home'
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('home/', views.home, name='home'),
+    path('', views.homepage_logged_out, name='homepage'), #homepage_logged_out
+    path('home/', views.homepage_logged_in, name='home'),   #homepage_logged_in
     path('about/', views.about, name='about'),
 ]
