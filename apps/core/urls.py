1aa46269be0a9339eb84fa4247df0e47f14ca281
Chad Sterling <cdsterling@gmail.com>
2019-12-10 18:16:49 -0800

    updating the urls.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('home_logged_in/', views.home_logged_in, name='home_logged_in')]
+    ]
